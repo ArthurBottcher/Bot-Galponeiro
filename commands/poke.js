b0aa15a37e750d0c8fc33440bc9b1c177535c093
Arthur Bottcher <50205007+ArthurBottcher@users.noreply.github.com>
2021-09-19 17:18:24 +0000

    fix bugs in poke cmd

--- commands/poke.js
+++ commands/poke.js
@@ -5,20 +5,20 @@ const axios = require('axios');
 exports.run = async (client, message, ) => {
 	message.delete();
 	let urlImage; 
-	const rand = Math.random(1,685); 
-	await axios.get(`https://pokeapi.co/api/v2/pokemon/${rand}`)
-		.then((res) => {
-			urlImage = res.data.sprites['other']['official-artwork']['front_default'];
-		})
-		.catch(() => {
-			message.reply('Ops');
-		});
-
+	const rand = Math.floor(Math.random()*685); 
+	const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${rand}`)
+	
+	urlImage = res.data.sprites.other["official-artwork"].front_default;
+	const specieData = await axios.get(res.data.species.url)
+   	let color = specieData.data.color.name.toUpperCase()
+	console.log(color)
+	if(color == 'PINK') color = '#FF64B4'
+	if(color == 'GRAY') color = '#808080'
 	if(urlImage != undefined){
 		let avatar = message.author.displayAvatarURL({format: 'png'});
 		const embed = new Discord.MessageEmbed()
-			.setColor('#f2eb24')
-			.setDescription('Seu gif: ')
+			.setColor(color)
+			.setDescription('Seu pokemon: '+ res.data.name)
 			.setImage(`${urlImage}`)
 			.setTimestamp()
 			.setFooter(rand)
