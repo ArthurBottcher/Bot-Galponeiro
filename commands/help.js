be2a3482cb72f1c77133a0caa863f5c4b39d6e61
ArthurBottcher <arthurbottcher@gmail.com>
2021-09-20 13:41:58 -0300

    finish help cmd

--- commands/help.js
+++ commands/help.js
@@ -1,61 +1,52 @@
-const Discord = require('discord.js')
-const algorithmia = require('algorithmia')
-const algorithmiaApiKey = process.env.ALGORITHMIA_API_KEY
-
-exports.run = async (client, message, args) => {
-	let tag = args.join(' ');
-	if (!tag) return message.reply('lembre-se de colocar uma tag')
-	const wiki = await getContent(tag)
+const Discord = require('discord.js');
+const fields = [
+	{name: '15hrs', value: '', inline: false},
+	{name: 'açai', value: '', inline: false},
+	{name: 'amanda', value: '', inline: false},
+	{name: 'aspirina', value: '', inline: false},
+	{name: 'bombom', value: '', inline: false},
+	{name: 'brunomars', value: '', inline: false},
+	{name: 'chatice', value: '', inline: false},
+	{name: 'coinflipt', value: 'Escolha cara ou coroa', inline: false},
+	{name: 'conselho', value: '', inline: false},
+	{name: 'dedo', value: '', inline: false},
+	{name: 'dev', value: '', inline: false},
+	{name: 'dianho', value: '', inline: false},
+	{name: 'ditado', value: '', inline: false},
+	{name: 'faustinho', value: '', inline: false},
+	{name: 'felipe', value: '', inline: false},
+	{name: 'freegame', value: 'Te mostra um jogo gratuito', inline: false},
+	{name: 'gif', value: 'Escreva um termo para buscar um gif', inline: false},
+	{name: 'gordo', value: 'Marque alguém', inline: false},
+	{name: 'insulina', value: '', inline: false},
+	{name: 'kiss', value: 'Marque alguem', inline: false},
+	{name: 'laranjinha', value: '', inline: false},
+	{name: 'marcelo', value: '', inline: false},
+	{name: 'megazord', value: '', inline: false},
+	{name: 'napratica', value: '', inline: false},
+	{name: 'norris', value: '', inline: false},
+	{name: 'number', value: 'Informe um número', inline: false},
+	{name: 'pato', value: 'Marque alguém', inline: false},
+	{name: 'poke', value: '', inline: false},
+	{name: 'pp', value: 'Piadas Pesadas', inline: false},
+	{name: 'ppt', value: 'Escolha: pedra, papel ou tessoura', inline: false},
+	{name: 'religion', value: '', inline: false},
+	{name: 'rufus', value: '', inline: false},
+	{name: 'say', value: 'Escreva algo para o bot dizer', inline: false},
+	{name: 'sumo', value: '', inline: false},
+	{name: 'tia', value: '', inline: false},
+	{name: 'tomate', value: '', inline: false},
+	{name: 'veio', value: '', inline: false},
+	{name: 'vote', value: '', inline: false},
+	{name: 'wiki', value: 'Escreva um termo para buscar no wikipédia', inline: false},
+];
+exports.run = async (client, message) => {
 	
 	const embed = new Discord.MessageEmbed()
-		.setColor('#f2eb24')
-		.setTitle(tag)
-		.setDescription(wiki)
-		.setTimestamp()
-	await message.channel.send(embed)	
-}
-
-async function getContent(searchTerm){
-    const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
-    const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
-    const wikipediaResponse = await wikipediaAlgorithm.pipe({
-        "lang": 'pt',
-        "articleName": searchTerm
-        })
-	try{
-    const wikipediaContent = wikipediaResponse.get()
-	searchTerm = ''
-	return sanitizeContent(wikipediaContent)
-	
-	}
-	catch(error){
-		searchTerm = ''
-		console.log(error)
-		console.log('Wiki: Termo não encontrado: ', searchTerm)
-		return 'Termo não encontrado'
-	}
-}
-
-function sanitizeContent(content){
-    const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.content)
-    const withoutDateInParentheses = removeDateInParentheses(withoutBlankLinesAndMarkdown)
-    
-
-    function removeBlankLinesAndMarkdown(text){
-        const allLines = text.split('\n')
-
-       const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
-            if (line.trim().length === 0 || line.trim().startsWith('=')){
-                return false
-            }
-            return true
-        })
-        return withoutBlankLinesAndMarkdown.join(' ')
-    }
-
-    function removeDateInParentheses(text){
-        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
-    }
-	
-    return withoutDateInParentheses.split(".", 4).join('. ')
-}
+		.setColor('#')
+		.setTitle('Meus comandos')
+		.setDescription('Fique a vontade para sugerir novos comandos, Meu prefixo é !')
+		.addFields(fields)
+		.setTimestamp();
+	await message.channel.send(embed);	
+};
